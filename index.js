afe301f2b6f92c90d242a424a1961bd61e2cd641
Matthew Burr <matt.d.burr@gmail.com>
2018-03-14 12:21:22 -0500

    Created build and src directories

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+app.use(express.static('build/public'));
 app.all('*', (req, res) =>
   res.sendFile('index.html', {
-    root: __dirname + '/public',
+    root: __dirname + '/build/public',
   }),
 );
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
