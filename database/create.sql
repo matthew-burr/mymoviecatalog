804a6d373ac9cefc5f9899a28f5009a0c6bc81b9
Matthew Burr <matt.d.burr@gmail.com>
2018-03-15 08:13:43 -0500

    Talent endpoints all working

--- database/create.sql
+++ database/create.sql
@@ -7,7 +7,36 @@ CREATE TABLE IF NOT EXISTS mmc.talent (
   UNIQUE (first_name, last_name)
 );
 
-TRUNCATE TABLE mmc.talent;
+DELETE FROM mmc.talent;
 INSERT INTO mmc.talent (first_name, last_name) VALUES
 ('Chris', 'Evans'),
 ('Chris', 'Hemsworth');
+
+CREATE TABLE IF NOT EXISTS mmc.movie (
+  id SERIAL NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL
+);
+
+DELETE FROM mmc.movie;
+INSERT INTO mmc.movie (title) VALUES
+('The Avengers'),
+('The Avengers: Age of Ultron'),
+('Captain America'),
+('Thor');
+
+CREATE TABLE IF NOT EXISTS mmc.movie_talent (
+  movie_id INT NOT NULL REFERENCES mmc.movie (id),
+  talent_id INT NOT NULL REFERENCES mmc.talent (id),
+  UNIQUE (movie_id, talent_id) 
+);
+
+DELETE FROM mmc.movie_talent;
+INSERT INTO mmc.movie_talent
+SELECT
+    m.id AS movie_id
+  , t.id AS talent_id
+  FROM mmc.movie AS m
+  CROSS JOIN mmc.talent AS t
+ WHERE (m.title LIKE 'The Avengers%')
+    OR (m.title = 'Captain America' AND t.last_name = 'Evans')
+    OR (m.title = 'Thor' AND t.last_name = 'Hemsworth');
--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('build/public'));
+
+// Talent endpoints
+// TODO: Look into nesting endpoints
 app.get('/talent', (req, res) => {
   talent
     .getTalent()
     .then(data => res.json(data))
-    .catch(error => console.log(error));
+    .catch(error => sendError(error));
 });
+app.get('/talent/:id', (req, res) => {
+  talent
+    .getTalentById(req.params.id)
+    .then(data => res.json(data))
+    .catch(error => sendError(error));
+});
+app.get('/talent/:id/movies', (req, res) => {
+  talent
+    .getTalentMovies(req.params.id)
+    .then(data => res.json(data))
+    .catch(error => sendError(error));
+});
+
+function sendError(res, error) {
+  console.log(error);
+}
 
 app.all('*', (req, res) =>
   res.sendFile('index.html', {
