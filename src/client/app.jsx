09be7ce3df2b81acd24d1208c2a836f2a4f4a292
Matthew Burr <matt.d.burr@gmail.com>
2018-03-21 08:33:49 -0500

    Moved raw index.html to React

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider, createStore } from 'react-redux';
+import Routing from './routing.jsx';
 
-const App = render(<Provider />, document.getElementById('app'));
+const App = render(<Routing />, document.getElementById('app'));
