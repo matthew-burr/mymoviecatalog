83969b1f489be9716276044cff9e39df3f2d6e2e
Matthew Burr <matt.d.burr@gmail.com>
2018-03-20 20:13:34 -0500

    Added webpack to prepare for working with React

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/client/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build/public/scripts'),
+    filename: 'mmc.bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.jsx$/, exclude: /(node_modules)/, use: 'babel-loader' }],
+  },
+};
